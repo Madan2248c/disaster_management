5e28f579cb228cab3ca800017ff65d48b2959502
Madan2248c <madangopalboddu123@gmail.com>
2023-10-13 16:24:40 +0530

    update VolunteerRegistration.dart

--- lib/VolunteerRegistration.dart
+++ lib/VolunteerRegistration.dart
@@ -1,60 +1,240 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
+
+const List<String> gender = <String>['Male', 'Female', 'Other'];
+String dropdownValue = "";
 
 class VolunteerRegistration extends StatefulWidget {
-  const VolunteerRegistration({super.key});
+  const VolunteerRegistration({Key? key}) : super(key: key);
 
   @override
   State<VolunteerRegistration> createState() => _VolunteerRegistrationState();
 }
 
+
 class _VolunteerRegistrationState extends State<VolunteerRegistration> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: SafeArea(
-          child: InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: const Icon(
-              Icons.arrow_back_outlined,
-              size: 30,
-            ),
-          ),
-        ),
-        title: const Text(
-          "ADMS",
-          style: TextStyle(color: Colors.black, fontFamily: 'Abyssinica_SIL'),
-        ),
-      ),
-      backgroundColor: Color.fromRGBO(86, 189, 84, 1),
-      body: Column(
-        children: [
-          Text(
-            "Complete Your Profile",
-            style: TextStyle(
-              fontFamily: 'poppins',
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'First Name',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: SafeArea(
+            child: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: const Icon(
+                Icons.arrow_back_outlined,
+                size: 30,
               ),
-            )
-          ])
-        ],
+            ),
+          ),
+          title: const Text(
+            "ADMS",
+            style: TextStyle(color: Colors.black, fontFamily: 'Abyssinica_SIL'),
+          ),
+        ),
+        backgroundColor: const Color.fromRGBO(86, 189, 84, 1),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Complete Your profile",
+                style: TextStyle(
+                  fontFamily: 'poppins',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width/2.5,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: Colors.white)
+                        ),
+                        hintText: "First Name",
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width/2.5,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(color: Colors.white)
+                        ),
+                        hintText: "Last Name",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DropdownMenu(
+                    dropdownMenuEntries: gender.map<DropdownMenuEntry<String>>((String value) {
+                    return DropdownMenuEntry<String>(value: value, label: value);
+                  }).toList(),
+                    onSelected: (String? value) {
+                      // This is called when the user selects an item.
+                      setState(() {
+                        dropdownValue = value!;
+                      });
+                    },
+                    hintText: 'Gender',
+                    inputDecorationTheme: InputDecorationTheme(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width/4,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(color: Colors.white)
+                        ),
+                        hintText: "Age",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30,right: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () {}, icon: Icon(Icons.pin_drop_outlined),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.white)
+                    ),
+                    hintText: "Location",
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30,right: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.call),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.white)
+                    ),
+                    hintText: "Contact Number",
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30,right: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.email_rounded),
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.white)
+                    ),
+                    hintText: "Email Id",
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Add Skills",
+                style: TextStyle(
+                  fontFamily: 'poppins',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30,right: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.white)
+                    ),
+                    hintText: "Enter the skills you have",
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 4, 20, 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.black,
+              ),
+              child: const Text(
+                "Sign Up",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
